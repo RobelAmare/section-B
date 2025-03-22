cf0202a896ccc9bca3f900b7e22be237c6edd947
Robel Amare <119671866+RobelAmare@users.noreply.github.com>
2025-03-22 20:13:30 +0300

    Create swapcase,find_and_index.py
    added the cleanest code for this topic

---
+++ Robel Amare/String_Method/swapcase, find and index/swapcase,find_and_index.py
@@ -0,0 +1,12 @@
+import string
+
+sentence = "THIS IS AN EXAMPLE"
+
+print(str.swapcase(sentence))  # thiss will output the given string in all lower case
+
+print(sentence.find("A"))   # outputs 8 since ther is a location for the given string
+print(sentece.find("a"))    # outputs -1 since ther is a letter in the given string
+
+
+print(sentence.index("A"))   # outputs 8 since ther is a location for the given string
+print(sentece.index("a"))   # outputs error message
