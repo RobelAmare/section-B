5aefdd910abf1ecd0d87fc688da4613d98e85e66
robel <amarerobel2@gmail.com>
2025-03-28 18:03:55 +0300

    v32

---
+++ Robel Amare/String_Method/replace, strip, lstrip/replace,stripandlstrip.py
@@ -0,0 +1,10 @@
+
+txt = "this is a test sentence "
+
+print(txt.replace("this","hello"))          # replaces the specified term / string
+
+print(txt.strip("this"))                    # removes the specified string
+
+print(txt.lstrip("this"))                   # >>
+
+print(txt.rstrip("this"))                   # >>
